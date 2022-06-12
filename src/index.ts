e37c36cd2baf01e3deb2e0cc7a141fb64ad48f65
Zorby <58777319+Zorby-dev@users.noreply.github.com>
2022-06-12 15:47:07 +0200

    behavior && commands

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,9 @@ document.addEventListener("keydown", (event) => {
     switch (event.code) {
     case "Enter":
         event.preventDefault();
-        shell.println(shell.input.get());
+        const input = shell.input.get();
+        shell.println(`<span>${input}</span>`);
+        shell.addToHistory(input);
         shell.handleCommand(shell.input.get());
         shell.clearInput();
         shell.print(prompt());
@@ -28,17 +30,25 @@ document.addEventListener("keydown", (event) => {
     case "ArrowUp":
         event.preventDefault();
         if (event.altKey) {
-            shell.shell.scrollBy(0, -100);
+            shell.shell.scrollBy(0, -300);
+        } else {
+            shell.shift();
         }
         break;
     case "ArrowDown":
         event.preventDefault();
         if (event.altKey) {
-            shell.shell.scrollBy(0, 100);
+            shell.shell.scrollBy(0, 300);
+        } else {
+            shell.unshift();
         }
         break;
     default:
-        console.log(event.code);
+        //console.log(event.code);
         break;
     }
+});
+
+document.addEventListener("contextmenu", (event) => {
+    event.preventDefault();
 });
--- src/shell.ts
+++ src/shell.ts
@@ -26,18 +26,49 @@ class Field {
     }
 }
 
+class InputField extends Field {
+    setCursor(position: number) {
+        const element = this.getElement();
+        let pos = position >= 0 ? position : element.innerHTML.length + position + 1;
+        let range = document.createRange();
+        let sel = window.getSelection()!;
+    
+        range.setStart(element.childNodes[0], pos);
+        range.collapse(true);
+    
+        sel.removeAllRanges();
+        sel.addRange(range);
+    }
+}
+
+function echo(shell: Shell, text: string) {
+    shell.println(text);
+}
+
+interface Command {
+    (shell: Shell, text: string): void
+}
+
 export class Shell {
+    static commands: {[cmdName: string]: Command} = {
+        "echo": echo
+    }
+
     shell: Element
     history: Field
-    input: Field
+    input: InputField
 
     user: string = "guest"
     cwd: string = "~"
 
+    private previous: string[] = []
+    private historyIndex: number = -1
+    private userInput: string = ""
+
     constructor() {
         this.shell = document.getElementById("shell")!;
         this.history = new Field(document.getElementById("history")!);
-        this.input = new Field(document.getElementById("input")!);
+        this.input = new InputField(document.getElementById("input")!);
     }
 
     print(text: string) {
@@ -65,7 +96,45 @@ export class Shell {
         this.shell.scrollTo(0, this.shell.scrollHeight);
     }
 
+    addToHistory(text: string) {
+        if (!text || this.previous.at(0) == text) return;
+        this.previous.unshift(text);
+        this.historyIndex = -1;
+    }
+
+    shift() {
+        if (this.historyIndex === -1) {
+            this.userInput = this.input.get();
+        }
+        if (this.historyIndex < this.previous.length - 1) {
+            this.historyIndex++;
+        }
+        this.input.set(this.previous.at(this.historyIndex)!);
+        this.input.setCursor(-1);
+    }
+
+    unshift() {
+        if (this.historyIndex === -1) {
+            this.userInput = this.input.get();
+        }
+        if (this.historyIndex >= 0) {
+            this.historyIndex--;
+        }
+        const val = this.historyIndex != -1 ? this.previous.at(this.historyIndex)! : this.userInput;
+        this.input.set(val);
+        this.input.setCursor(-1);
+    }
+
     handleCommand(command: string) {
-        
+        const match = /(\S+)(?: |$)(.*)$/.exec(command);
+        if (!match) return;
+        const cmdName = match[1];
+        const args = match[2];
+        const cmd = Shell.commands[cmdName];
+        if (!cmd) {
+            this.println(`'${cmdName}' is not recognised as an internal or external command, operable program or batch file.`);
+            return;
+        }
+        cmd(this, args);
     }
 }
